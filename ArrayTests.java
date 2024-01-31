8de473fa9cd736e26ca4dadbe1b4fd9c1bb0ebc4
shenovad <114978065+shenovad@users.noreply.github.com>
2024-01-31 10:40:32 -0800

    changed reverseInPlace

--- ArrayTests.java
+++ ArrayTests.java
@@ -7,6 +7,19 @@ public class ArrayTests {
     int[] input1 = { 3 };
     ArrayExamples.reverseInPlace(input1);
     assertArrayEquals(new int[]{ 3 }, input1);
+
+    int[] input2 = {1,2,3,4 };
+    ArrayExamples.reverseInPlace(input2);
+    assertArrayEquals(new int[]{4,3,2,1}, input2);
+
+    int[] input3 = {1,2,3};
+    ArrayExamples.reverseInPlace(input3);
+    assertArrayEquals(new int[]{3,2,1}, input3);
+
+    int[] input4 = {};
+    ArrayExamples.reverseInPlace(input4);
+    assertArrayEquals(new int[]{}, input4);
+
 	}
 
 
